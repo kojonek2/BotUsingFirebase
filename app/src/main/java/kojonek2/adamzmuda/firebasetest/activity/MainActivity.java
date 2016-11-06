5543ce12ff62f21ffa13a9d3923581923101b24a
kojonek2 <kojonek2@wp.pl>
2016-11-06 12:37:57 +0100

    Implemented Jquery

--- app/src/main/java/kojonek2/adamzmuda/firebasetest/activity/MainActivity.java
+++ app/src/main/java/kojonek2/adamzmuda/firebasetest/activity/MainActivity.java
@@ -1,13 +1,23 @@
-package kojonek2.adamzmuda.firebasetest;
+package kojonek2.adamzmuda.firebasetest.activity;
 
 import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.ValueCallback;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import kojonek2.adamzmuda.firebasetest.R;
+import kojonek2.adamzmuda.firebasetest.helper.Jquery;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +25,12 @@ public class MainActivity extends AppCompatActivity {
     TextView informationText;
     WebView webView;
     Button confirmBtn;
-    String strJquery;
+
+    String question;
+    String answer;
+
+    DatabaseReference myReference;
+    DatabaseReference questionReference;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -26,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
         informationText = (TextView) findViewById(R.id.informationText);
         webView = (WebView) findViewById(R.id.webView);
         confirmBtn = (Button) findViewById(R.id.confirmButton);
+
+        myReference = FirebaseDatabase.getInstance().getReference().child("Question");
 
         webView.setWebViewClient(new WebViewClient() {
             @Override
@@ -51,8 +68,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private int checkForAnswer() {
+        webView.evaluateJavascript(new Jquery().getJqueryCode(), null);
+        webView.evaluateJavascript("$(\"p\").text()", new ValueCallback<String>() {
+            @Override
+            public void onReceiveValue(String value) {
+                question = value;
+                questionReference = myReference.child(question);
+                questionReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()){
+                            //knows answer
+
+                            selectAnswer(answer);
+                        } else {
+                            //doesn't know answer
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) { }
+
+                });
+
+            }
+        });
 
         return 0;
+    }
+
+    private void selectAnswer(String answerPassed) {
+
     }
 
     public void onConfirmBtnClicked(View view) {
