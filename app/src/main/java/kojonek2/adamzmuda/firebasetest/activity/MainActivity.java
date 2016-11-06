fc30af3f20f7f5f8ff30055121ded43b5b94423d
kojonek2 <kojonek2@wp.pl>
2016-11-06 15:59:55 +0100

    Checking answers

--- app/src/main/java/kojonek2/adamzmuda/firebasetest/activity/MainActivity.java
+++ app/src/main/java/kojonek2/adamzmuda/firebasetest/activity/MainActivity.java
@@ -22,12 +22,19 @@ import kojonek2.adamzmuda.firebasetest.helper.Jquery;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String STATE_REQUESTING_ANSWER = "requestingAnswer";
+    static final String STATE_CHECK_CORRECT_OF_ANSWER = "checkCorrectOfAnswer";
+
+
     TextView informationText;
     WebView webView;
     Button confirmBtn;
 
     String question;
     String answer;
+
+    Boolean requestingAnswer;
+    Boolean checkCorrectOfAnswer;
 
     DatabaseReference myReference;
     DatabaseReference questionReference;
@@ -44,16 +51,23 @@ public class MainActivity extends AppCompatActivity {
 
         myReference = FirebaseDatabase.getInstance().getReference().child("Question");
 
+
+        requestingAnswer = savedInstanceState != null && savedInstanceState.getBoolean(STATE_REQUESTING_ANSWER);
+        checkCorrectOfAnswer = savedInstanceState != null && savedInstanceState.getBoolean(STATE_CHECK_CORRECT_OF_ANSWER);
+
+
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
                 switch (url) {
+                    //TODO: Dodać co ma zrobic jesli odpowiedz jest poprawna lub niepoprawna.
                     case "http://nauczyciel.edu.pl/login.php":
                         informationText.setText(R.string.please_login);
                         break;
                     case "http://nauczyciel.edu.pl/user.php?page=pytania_online&arg=1115":
-                            checkForAnswer();
+                        informationText.setText(R.string.information);
+                        checkForAnswer();
                         break;
                     default:
                         webView.loadUrl("http://nauczyciel.edu.pl/user.php?page=pytania_online&arg=1115");
@@ -65,6 +79,14 @@ public class MainActivity extends AppCompatActivity {
 
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("http://nauczyciel.edu.pl/user.php?page=pytania_online&arg=1115");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putBoolean(STATE_REQUESTING_ANSWER, requestingAnswer);
+        outState.putBoolean(STATE_CHECK_CORRECT_OF_ANSWER, checkCorrectOfAnswer);
     }
 
     private void checkForAnswer() {
@@ -79,11 +101,10 @@ public class MainActivity extends AppCompatActivity {
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         if (dataSnapshot.exists()){
                             //knows answer
-
+                            answer = dataSnapshot.getValue().toString();
                             selectAnswer(answer);
                         } else {
                             //doesn't know answer
-
                             requestAnswerFromUser();
                         }
                     }
@@ -98,14 +119,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void requestAnswerFromUser() {
-
+        informationText.setText(R.string.give_answer);
+        requestingAnswer = true;
     }
 
     private void selectAnswer(final String answerPassed) {
         webView.evaluateJavascript("$(\"td\").eq(1).text()", new ValueCallback<String>() {
             @Override
             public void onReceiveValue(String value) {
-                value = value.substring(18, value.length() - 5);
                 if(value.equals(answerPassed)) {
                     webView.evaluateJavascript("$(\"input\").get(4).click(); $(\"input\").get(10).click();", null);
                 }
@@ -138,6 +159,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onConfirmBtnClicked(View view) {
-
+        if(requestingAnswer) {
+            webView.evaluateJavascript("$(\"input\").get(10).click();", null);
+            checkCorrectOfAnswer = true;
+        }
     }
 }
