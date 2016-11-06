4d350c1ee6721ab5594dcf8b89c7fa360a0024ec
kojonek2 <kojonek2@wp.pl>
2016-11-06 09:36:53 +0100

    Ready Firedatabase

--- app/src/main/java/kojonek2/adamzmuda/firebasetest/MainActivity.java
+++ app/src/main/java/kojonek2/adamzmuda/firebasetest/MainActivity.java
@@ -4,17 +4,18 @@ import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     TextView informationText;
     WebView webView;
     Button confirmBtn;
+    String strJquery;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -26,19 +27,35 @@ public class MainActivity extends AppCompatActivity {
         webView = (WebView) findViewById(R.id.webView);
         confirmBtn = (Button) findViewById(R.id.confirmButton);
 
-        webView.setWebChromeClient(new WebChromeClient() {
-            @SuppressLint("SetTextI18n")
+        webView.setWebViewClient(new WebViewClient() {
             @Override
-            public void onProgressChanged(WebView view, int newProgress) {
-                super.onProgressChanged(view, newProgress);
-                informationText.setText(Integer.toString(newProgress));
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                switch (url) {
+                    case "http://nauczyciel.edu.pl/login.php":
+                        informationText.setText(R.string.please_login);
+                        break;
+                    case "http://nauczyciel.edu.pl/user.php?page=pytania_online&arg=1115":
+                            checkForAnswer();
+                        break;
+                    default:
+                        webView.loadUrl("http://nauczyciel.edu.pl/user.php?page=pytania_online&arg=1115");
+                        break;
+
+                }
             }
         });
 
-        webView.setWebViewClient(new WebViewClient());
-
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("http://nauczyciel.edu.pl/user.php?page=pytania_online&arg=1115");
+    }
+
+    private int checkForAnswer() {
+
+        return 0;
+    }
+
+    public void onConfirmBtnClicked(View view) {
 
     }
 }
